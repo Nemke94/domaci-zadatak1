acc02ed8d426bdf8fda45ad33b70623658827485
Nemanja Grujić <nemanjagrujic1994@gmail.com>
2017-09-08 01:26:35 +0200

    Add files via upload

---
+++ prvidomaci.js
@@ -0,0 +1,14 @@
+function broj(N) 
+{
+  var total = 0;
+    for(var i = 1; i <= N; i++)
+    {
+      total += i;
+    }
+    return total;
+}
+
+function run(){
+  val = document.getElementById("val").value;
+  document.getElementById("results").innerHTML=val+": "+broj(val)
+  }
